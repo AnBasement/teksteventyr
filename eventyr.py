fd1861b901b73c2a5397c60ff4491815a7e92d9a
AnBasement <KristofferAG@gmail.com>
2025-08-16 23:31:02 +0200

    Legger inn kommando for å hente ut hvilke tall som er funnet og et par andre småfiks.

--- eventyr.py
+++ eventyr.py
@@ -24,7 +24,7 @@ ugyldig = "Ugyldig svar, prøv igjen. "
 omstart = "Vil du spille igjen? (ja/nei) "
 tap = "Kjellerbeistet tok deg, og omverdenen hører aldri fra deg igjen."
 spiller_prompt = "Hva vil du gjøre? "
-hjelp = "Spillet liker bare ett-ords kommandoer – hold det enkelt!\nFor å bevege deg fra rom til rom kan du bruke himmelretningene (nord, sør, sørvest etc.).\nOm du har glemt hvor du er, kan du skrive 'utforsk' for en påminnelse."
+hjelp = "Spillet liker bare ett-ords kommandoer – hold det enkelt!\nFor å bevege deg fra rom til rom kan du bruke himmelretningene (nord, sør, sørvest etc.).\nOm du har glemt hvor du er, kan du skrive 'utforsk' for en påminnelse, eller 'tallkode' for en påminnelse om koden!"
 
 # Variabel for rommet man befinner seg i. Begynner på "rom1" og endres avhengig av brukers valg.
 rom = "rom1"
@@ -54,13 +54,13 @@ rom4_inngang_tekst = (
     "Idet du går gjennom den tomme dørkarmen i vest, kjenner du frisk luft treffe deg i ansiktet."
 )
 gang1_inngang_tekst = (
-    "Du prøver døren, som lett gir etter./n"
+    "Du prøver døren, som lett gir etter.\n"
     "Du blir møtt av en varm, tung og klam luft."
 )
-rom6_inngang_tekst = (
+rom5_inngang_tekst = (
     "Døren har to separate låser, men idet du vrir om håndtaket gir den lett etter."
 )
-rom7_inngang_tekst = (
+rom6_inngang_tekst = (
     "Det kommer en relativt sterk, men frisk lukt fra døren, som blir forklart idet du åpner den."
 )
 
@@ -86,21 +86,21 @@ gang1_utforsk_tekst = (
     "Gangen strekker seg fra vest til øst, men du ser ingen andre dører.\n" \
     "Gangen er opplyst av spottere, og fremstår ellers som helt bar."
 )
-rom6_utforsk_tekst = (
-    "Du finner deg omringet av høye stabler av kasser og esker. To høye reoler står midt i rommet, og langs den bakre veggen står et enkelt skap."
+rom5_utforsk_tekst = (
+    "Du finner deg omringet av høye stabler av kasser og esker. To høye reoler står midt i rommet, og langs den bakre veggen står et enkelt skap. En dør leder sør."
 )
-rom7_utforsk_tekst = (
+rom6_utforsk_tekst = (
     "Du står i noe som ligner på en vaskekjeller. Det står to oppvaskamaskiner stablet opp langs den ene veggen.\n" \
     "Den bakre veggen og taket er fullt av rør, noen med små lekkasjer og andre så rustne at det er et under de ikke lekker. På ett av rørene er det et stort rødt hjul.\n" \
-    "Langs den siste veggen står en rekke hyller med forskjellige vaskemidler på."
+    "Langs den siste veggen står en rekke hyller med forskjellige vaskemidler på. En dør leder sør."
 )
 
 # Funksjon for beskrivelse av rom
 def rombeskrivelse(romnavn, inngang, utforsk, besøkt):
     if not besøkt[romnavn]:
-        print(inngang)
-        print(utforsk)
+        print(inngang)       # Printes kun første gang
         besøkt[romnavn] = True
+    print(utforsk)            # Printes hver gang spilleren går inn
     return besøkt
 
 # Dict med oversikt over besøkte rom
@@ -110,26 +110,46 @@ besøkt = {
     "rom3": False,
     "rom4": False,
     "gang1": False,
-    "rom6": False,
-    "rom7": False
+    "rom5": False,
+    "rom6": False
 }
 
 # Dict for interagerbare objekter
 status = {
     "har_hammer": False,
     "har_brekkjern": False,
-    "har_nøkkel": False
+    "har_nøkkel": False,
+    "rom3_skap": False,
+    "tall1": False,
+    "tall2": False,
+    "tall3": False
 }
+
+# Funksjon som lister opp tallene spilleren har funnet
+def tallkode_funnet(status):
+    funnede_tall = []
+    if status["tall1"]:
+        funnede_tall.append("4")
+    if status["tall2"]:
+        funnede_tall.append("7")
+    if status["tall3"]:
+        funnede_tall.append("2")
+
+    if funnede_tall:
+        print("Så langt har du funnet tallene: " + ", ".join(funnede_tall))
+    else:
+        print("Du har ikke funnet noen tall enda!")
+
 
 # Dict for gyldige valg i hvert rom
 gyldige_valg_i_rom = {
-    "rom1": ["vindu", "øst", "utforsk", "hjelp"],
-    "rom2": ["nord", "skap", "arbeidsbenk", "øst", "utforsk", "hjelp"],
-    "rom3": ["sør", "vest", "bokser", "malingsspann", "utforsk", "hjelp"],
-    "rom4": ["øst", "trapp", "skrivebord", "utforsk", "hjelp"],
-    "gang1": ["sør", "nordøst", "nordvest", "utforsk", "hjelp"],
-    "rom6": ["sør", "esker", "hyller", "skap", "utforsk", "hjelp"],
-    "rom7": ["sør", "vaskemaskiner", "vaskemaskin", "hyller", "rør", "hjul", "utforsk", "hjelp"]
+    "rom1": ["vindu", "øst", "utforsk", "hjelp", "tallkode"],
+    "rom2": ["nord", "skap", "arbeidsbenk", "øst", "utforsk", "hjelp", "tallkode"],
+    "rom3": ["sør", "vest", "nord", "bokser", "malingsspann", "utforsk", "hjelp", "tallkode"],
+    "rom4": ["øst", "trapp", "skrivebord", "utforsk", "hjelp", "tallkode"],
+    "gang1": ["sør", "nordøst", "nordvest", "utforsk", "hjelp", "tallkode"],
+    "rom5": ["sør", "esker", "hyller", "skap", "utforsk", "hjelp", "tallkode"],
+    "rom6": ["sør", "vaskemaskiner", "vaskemaskin", "hyller", "rør", "hjul", "utforsk", "hjelp", "tallkode"]
 }
 
 # Funksjon som nullstiller besøkte rom
@@ -137,11 +157,13 @@ def nullstill_rom(romnavn, besøkt):
     besøkt[romnavn] = False
 
 # Funksjon for å håndtere hjelp og utforsk
-def hjelp_og_utforsk(valg, hjelp_tekst, utforsk_tekst):
+def hjelp_og_utforsk(valg, hjelp_tekst, utforsk_tekst, status):
     if valg == "hjelp":
         print(hjelp_tekst)
     elif valg == "utforsk":
         print(utforsk_tekst)
+    elif valg == "tallkode":
+        tallkode_funnet(status)
 
 # Funksjon for når spilleren taper spillet
 def tap_restart(tapmelding):
@@ -149,54 +171,56 @@ def tap_restart(tapmelding):
     while True:
         valg_restart = input(omstart).strip().lower()
         if valg_restart == "ja":
-            return True
+            return True, "rom1"
         elif valg_restart == "nei":
             quit()
         else:
             print(ugyldig)
 
 # Funksjon for rom1
-def rom_1(rom, restart, besøkt):
+def rom1(rom, restart, besøkt):
+    besøkt = rombeskrivelse("rom1", rom1_inngang_tekst, rom1_utforsk_tekst, besøkt)
     while True: # Separat løkke for rom1
-        besøkt = rombeskrivelse("rom1", rom1_inngang_tekst, rom1_utforsk_tekst, besøkt)
 
         valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom1"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, rom1_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, rom1_utforsk_tekst, status)
             continue
 
         if valg == "øst":
             rom = "rom2"
             break  # går videre til rom2
         elif valg == "vindu":
-            restart = tap_restart("Du klatrer stigen opp mot vinduet. Idet du når toppen, knekker den sammen under deg. Du faller på nakken og alt går umiddelbart i svart.")
+            restart, rom = tap_restart("Du klatrer stigen opp mot vinduet. Idet du når toppen, knekker den sammen under deg. Du faller på nakken og alt går umiddelbart i svart.")
             break  # bryt rom1-løkka for å håndtere restart eller quit
         else:
             print(ugyldig)
     return rom, restart, besøkt
 
 # Funksjon for rom2
-def rom_2(rom, restart, status, besøkt):
+def rom2(rom, restart, status, besøkt):
+    besøkt = rombeskrivelse("rom2", rom2_inngang_tekst, rom2_utforsk_tekst, besøkt)
     while True:  # separat løkke for rom2
-        besøkt = rombeskrivelse("rom2", rom2_inngang_tekst, rom2_utforsk_tekst, besøkt)
 
         valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom2"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, rom2_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, rom2_utforsk_tekst, status)
             continue
         
         if valg == "nord":
             rom = "rom3"
-            nullstill_rom("rom2", besøkt)
             break  # gå videre til rom3
         elif valg == "skap":
-            print("Du åpner skapdørene, og støkker idet et rustent strykebrett faller over deg.\nEtter at nervene roer seg kikker du rundt i skapet, men ser ingenting av interesse.")
+            if not status["rom3_skap"]:
+                print("Du åpner skapdørene, og støkker idet et rustent strykebrett faller over deg.\nEtter at nervene roer seg kikker du rundt i skapet, men ser ingenting av interesse.")
+            else:
+                print("Vaskebrettet ligger på gulvet, resten av skapet er uinteressant.")
         elif valg == "arbeidsbenk":
             rom2_arbeidsbenk(status)
         elif valg == "øst":
-            restart = rom2_ost(restart)
+            restart, rom = rom2_ost(restart)
             if restart:
                 break  # bryt rom2-løkka for å håndtere restart eller quit
         else:
@@ -224,39 +248,38 @@ def rom2_ost(restart):
     while True:
         svar = input("Etterhvert som du nærmer deg døren på østveggen hører du bevegelse og noe som høres ut som grynting på andre siden av døren. Vil du åpne den? (ja/nei) ").lower().strip()
         if svar == "ja":
-            restart = tap_restart(tap)
-            return restart
+            restart, rom = tap_restart(tap)
+            return restart, rom
         elif svar == "nei":
             break  # tilbake til rom2 uten å gjøre noe
         else:
             print(ugyldig)
 
 # Funksjon for rom 3
-def rom_3(rom, restart, status, besøkt):
+def rom3(rom, restart, status, besøkt):
+    besøkt = rombeskrivelse("rom3", rom3_inngang_tekst, rom3_utforsk_tekst, besøkt)
     while True: # Løkke for rom 3
-        besøkt = rombeskrivelse("rom3", rom3_inngang_tekst, rom3_utforsk_tekst, besøkt)
 
         valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom3"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, rom3_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, rom3_utforsk_tekst, status)
             continue
 
         if valg == "sør":
             rom = "rom2"
-            nullstill_rom("rom3", besøkt) # nullstill for neste gang
-            break  # gå tilbake til rom2
+            break
         elif valg == "malingsspann":
             rom3_malingsspann(status)
         elif valg == "bokser":
             print("En haug med det som ser ut til å en gang ha vært pappesker, alle merket med tallet '4', men er nå en haug med forråtnende papp.")
+            status["tall1"] = True
         elif valg == "vest":
             rom = "rom4"
-            nullstill_rom("rom3", besøkt) # nullstill for neste gang
-            break  # gå videre til rom4
+            break
         elif valg == "nord":
             rom = "gang1"
-            nullstill_rom("rom3", besøkt) # nullstill for neste gang
+            break
     return rom, restart, status, besøkt
 
 # Funksjon for malingsspann i rom 3
@@ -282,14 +305,14 @@ def rom3_malingsspann(status):
     return status # Returnerer oppdatert status
 
 # Funksjon for rom4
-def rom_4(rom, restart, status, besøkt):
+def rom4(rom, restart, status, besøkt):
+    besøkt = rombeskrivelse("rom4", rom4_inngang_tekst, rom4_utforsk_tekst, besøkt)
     while True:  # Løkke for rom 4
-        besøkt = rombeskrivelse("rom4", rom4_inngang_tekst, rom4_utforsk_tekst, besøkt)
 
         valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom4"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, rom4_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, rom4_utforsk_tekst, status)
             continue
 
         if valg == "trapp":
@@ -300,7 +323,6 @@ def rom_4(rom, restart, status, besøkt):
             rom4_skrivebord(status)
         elif valg == "øst":
             rom = "rom3"
-            nullstill_rom("rom4", besøkt)  # nullstill for neste gang
             break  # gå videre til rom3
     return rom, restart, status, besøkt
 
@@ -343,44 +365,40 @@ def rom4_skrivebord(status):
     return status
 
 # Funksjon for gang1
-def gang_1(rom, restart, status, besøkt):
+def gang1(rom, restart, status, besøkt):
+    besøkt = rombeskrivelse("gang1", gang1_inngang_tekst, gang1_utforsk_tekst, besøkt)
     while True:  # Løkke for gang 1
-        besøkt = rombeskrivelse("gang1", gang1_inngang_tekst, gang1_utforsk_tekst, besøkt)
 
         valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["gang1"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, gang1_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, gang1_utforsk_tekst, status)
             continue
 
         if valg == "sør":
             rom = "rom3"
-            nullstill_rom("gang1", besøkt)  # nullstill for neste gang
             break  # gå videre til rom3
         elif valg == "nordvest":
-            rom = "rom7"
-            nullstill_rom("gang1", besøkt)  # nullstill for neste gang
-            break  # gå videre til rom7
-        elif valg == "nordøst":
             rom = "rom6"
-            nullstill_rom("gang1", besøkt)  # nullstill for neste gang
             break  # gå videre til rom6
+        elif valg == "nordøst":
+            rom = "rom5"
+            break  # gå videre til rom5
     return rom, restart, status, besøkt
 
-# Funksjon for rom6
-def rom6(rom, restart, status, besøkt):
-    while True:  # Løkke for rom6
-        besøkt = rombeskrivelse("rom6", rom6_inngang_tekst, rom6_utforsk_tekst, besøkt)
+# Funksjon for rom5
+def rom5(rom, restart, status, besøkt):
+    besøkt = rombeskrivelse("rom5", rom5_inngang_tekst, rom5_utforsk_tekst, besøkt)
+    while True:  # Løkke for rom5
 
-        valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom6"], ugyldig)
+        valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom5"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, rom6_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, rom5_utforsk_tekst, status)
             continue
 
         if valg == "sør":
             rom = "gang1"
-            nullstill_rom("rom6", besøkt)  # nullstill for neste gang
             break  # gå videre til gang
         elif valg == "esker":
             print("Du ser over stablene av esker som står spredd langs veggene, i varierende grad av forråtnelse.\n" \
@@ -391,42 +409,45 @@ def rom6(rom, restart, status, besøkt):
         elif valg == "skap":
             print("Bakerst i rommet står det et skap som minner deg om skoledagene dine. Sett bort fra tallet '7' på forsiden er det ingenting uvanlig med det.\n" \
             "Du tar tak i håndtaket og røsker til, men skapet beveger seg ikke.")
+            status["tall2"] = True
     return rom, restart, status, besøkt
 
-# Funksjon for rom7
-def rom7(rom, restart, status, besøkt):
-    while True: # Løkke for rom7
-        besøkt = rombeskrivelse("rom7", rom7_inngang_tekst, rom7_utforsk_tekst, besøkt)
+# Funksjon for rom6
+def rom6(rom, restart, status, besøkt):
+    besøkt = rombeskrivelse("rom6", rom6_inngang_tekst, rom6_utforsk_tekst, besøkt)
+    while True: # Løkke for rom6
 
-        valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom7"], ugyldig)
+        valg = sjekk_gyldig_valg(spiller_prompt, gyldige_valg_i_rom["rom6"], ugyldig)
 
-        if valg in ["hjelp", "utforsk"]:
-            hjelp_og_utforsk(valg, hjelp, rom7_utforsk_tekst)
+        if valg in ["hjelp", "utforsk", "tallkode"]:
+            hjelp_og_utforsk(valg, hjelp, rom6_utforsk_tekst, status)
             continue
 
         if valg == "sør":
             rom = "gang1"
-            nullstill_rom("rom7", besøkt) # Nullstill for neste gang
             break # Gå tidere til gang
         elif valg in ["vaskemaskiner", "vaskemaskin"]:
             print("Vaskemaskinene er i dårlig stand, og tydeligvis ikke brukt på en stund. Det ligger en haug med råtne klær i den ene, og ingen av dem reagerer når du trykker på knappene.")
         elif valg == "hyller":
             print("Du ser fort over hyllene etter noe som kan hjelpe deg, men ser stort sett bare forskjellige vaskemidler av merket '2MO'.")
+            status["tall3"] = True
         elif valg in ["rør", "hjul"]:
-            rom7_hjul(status)
+            restart, rom = rom6_hjul(restart)
+            if restart:
+                break
     return rom, restart, status, besøkt
 
-# Funksjon for å velge hjul eller rør i rom 7
-def rom7_hjul(restart):
+# Funksjon for å velge hjul eller rør i rom 6
+def rom6_hjul(restart):
     while True:
         svar = input("Det største av rørene har et rødt hjul festet til seg. Du tar borti røret, og kan kjenne at det beveger seg vann gjennom det. Vil du vri på hjulet? (ja/nei) ").lower().strip()
         if svar == "ja":
-            restart = tap_restart("Med noe makt klarer du å vri om på hjulet. Det begynner umiddelbart å fosse vann ut av vaskemaskinene, og et gitter smeller ned foran døren.\n" \
+            restart, rom = tap_restart("Med noe makt klarer du å vri om på hjulet. Det begynner umiddelbart å fosse vann ut av vaskemaskinene, og et gitter smeller ned foran døren.\n" \
             "Du prøver desperat å snu hjulet tilbake, men det sitter låst fast. Du forsøker å rive opp gitteret, men det rikker seg ikke.\n" \
             "Det tar bare noen sekunder før rommet er fylt med vann. Uten luft svelger du ned vann, og alt går i sort.")
-            return restart
+            return restart, rom
         elif svar == "nei":
-            break  # tilbake til rom7 uten å gjøre noe
+            return False, "rom6" 
         else:
             print(ugyldig)
 
@@ -456,16 +477,16 @@ while True:
 
     # Hovedløkke for å navigere gjennom rommene
     if rom == "rom1":
-        rom, restart, besøkt = rom_1(rom, restart, besøkt)
+        rom, restart, besøkt = rom1(rom, restart, besøkt)
     elif rom == "rom2":
-        rom, restart, status, besøkt = rom_2(rom, restart, status, besøkt)
+        rom, restart, status, besøkt = rom2(rom, restart, status, besøkt)
     elif rom == "rom3":
-        rom, restart, status, besøkt = rom_3(rom, restart, status, besøkt)
+        rom, restart, status, besøkt = rom3(rom, restart, status, besøkt)
     elif rom == "rom4":
-        rom, restart, status, besøkt = rom_4(rom, restart, status, besøkt)
+        rom, restart, status, besøkt = rom4(rom, restart, status, besøkt)
     elif rom == "gang1":
-        rom, restart, status, besøkt = rom_4(rom, restart, status, besøkt)
+        rom, restart, status, besøkt = gang1(rom, restart, status, besøkt)
+    elif rom == "rom5":
+        rom, restart, status, besøkt = rom5(rom, restart, status, besøkt)
     elif rom == "rom6":
-        rom, restart, status, besøkt = rom_4(rom, restart, status, besøkt)
-    elif rom == "rom7":
-        rom, restart, status, besøkt = rom_4(rom, restart, status, besøkt)
+        rom, restart, status, besøkt = rom6(rom, restart, status, besøkt)
